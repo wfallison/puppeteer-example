f9aed38566803fdcadcf432da342b5198b5c2d26
Billy Allison <ballison@macguyvertech.com>
2023-08-30 11:16:57 -0700

    Adds CLI args for URL

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const puppeteer = require('puppeteer');
+const url = process.argv[2];
+
 
 (async () => {
+  if (!url) {
+    throw new Error('Absolute URI, including scheme, is required as command line argument')
+  }
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const now = new Date();
-  await page.goto('http://localhost:3000');
+  await page.goto(url);
   await page.pdf({
     path: `reports-out/${now.getTime()}-report-out.pdf`,
   });
